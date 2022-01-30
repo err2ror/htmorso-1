459e876376f749b0a000993d14a862c6086c22b1
orsoxi <orsod@mailforkids.net>
2022-01-30 19:58:46 +0100

    I

---
+++ orso1/js/script.js
@@ -0,0 +1,13 @@
+function doso(){
+if (login < 1){
+let info = document.getElmentById("un").innerHtml
+function getelbyid(elid){
+
+}
+alert("Hi, user thanks for sign up/login!");
+alert("Switch to file:///home/orso/Bureau/htmorso/orso1.html");
+let login = 1;
+} else { 
+let login = 1;
+};
+};
---
+++ script.js
@@ -0,0 +1,4 @@
+function doso(){
+alert("Hi, user thanks for sign up/login!")
+}
+console.log("hi")
